45e2c0b5ba817c74f85f9c3124496569a03712d0
dohoan259 <dohoan259@gmail.com>
2022-07-07 19:38:20 +0700

    refs#2 - fix movie detail

--- lib/movie_detail_page.dart
+++ lib/movie_detail_page.dart
@@ -1,10 +1,12 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MovieDetailPage extends StatelessWidget {
   const MovieDetailPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Text("Movie detail"),
+    );
   }
 }
